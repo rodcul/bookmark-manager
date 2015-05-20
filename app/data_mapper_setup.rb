7e14ceb379c67e45e7829a610f9eafb4f793326d
Rodney Cullen <rodcul@gmail.com>
2015-05-20 23:23:44 +0100

    Added require_relative models to data_mapper_setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,5 +1,10 @@
 env = ENV['RACK_ENV'] || 'development'
 
+
+require_relative 'models/link'
+require_relative 'models/tag'
+require_relative 'models/user'
+
 # we're telling datamapper to use a postgres database on localhost.
 # The name will be "bookmark_manager_test" or "bookmark_manager_development"
 # depending on the environment
