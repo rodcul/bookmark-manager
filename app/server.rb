36bc7f62adfb94afd4c37c1896c19cb29d647c5f
Rodney Cullen <rodcul@gmail.com>
2015-05-21 13:28:13 +0100

    Got rspec working

--- app/server.rb
+++ app/server.rb
@@ -2,12 +2,15 @@ require 'sinatra'
 require 'data_mapper'
 require 'rack-flash'
 require 'sinatra/partial'
-require_relative 'data_mapper_setup'
-require_relative 'helpers/application'
+
+
 
 require_relative 'models/link' # this needs to be done after datamapper is initialised
 require_relative 'models/tag'
 require_relative 'models/user'
+
+require_relative 'helpers/application'
+require_relative 'data_mapper_setup'
 
 require_relative 'controllers/users'
 require_relative 'controllers/sessions'
